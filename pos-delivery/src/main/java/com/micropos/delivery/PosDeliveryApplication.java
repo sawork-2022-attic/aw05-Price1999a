e250dc3b1bca6939f3a8b7d2260a5106e4a0dd24
Shen Tianqi <1055862487@qq.com>
2022-05-06 15:49:40 +0800

    快递模块第一部分完成
    基本架构已经搭建 下一个目标是处理cart中的输出

---
+++ pos-delivery/src/main/java/com/micropos/delivery/PosDeliveryApplication.java
@@ -0,0 +1,16 @@
+package com.micropos.delivery;
+
+import com.micropos.delivery.model.Product;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PosDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosDeliveryApplication.class, args);
+    }
+
+}
